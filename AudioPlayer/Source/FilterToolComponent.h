bd4b2089a2c6a773272e3b178ca8c2fc8ba0eb39
0ls3n <Rasmus782@gmail.com>
2025-06-26 12:09:21 +0200

    Changed header file in FilterToolComponent.h
    I changed the header in the FilterToolComponent header class, to not use the AudioProcessor header file, but use the SoloFilterProcessing header instead.

--- AudioPlayer/Source/FilterToolComponent.h
+++ AudioPlayer/Source/FilterToolComponent.h
@@ -11,7 +11,7 @@
 #pragma once
 
 #include <JuceHeader.h>
-#include "PluginProcessor.h"
+#include "SoloFilterProcessing.h"
 #include "ApplicationColours.h"
 
 //==============================================================================
