4d2e6f9a40e943df5f8893cbe87dc2cf89b9f28b
0ls3n <Rasmus782@gmail.com>
2025-06-30 19:49:28 +0200

    Updated Company Name
    I updated the company name in the Projucer.

--- AudioPlayer/JuceLibraryCode/JuceHeader.h
+++ AudioPlayer/JuceLibraryCode/JuceHeader.h
@@ -42,7 +42,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "JustReference";
-    const char* const  companyName    = "";
+    const char* const  companyName    = "JustMixing";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
--- AudioPlayer/JuceLibraryCode/JucePluginDefines.h
+++ AudioPlayer/JuceLibraryCode/JucePluginDefines.h
@@ -47,10 +47,10 @@
  #define JucePlugin_Desc                   "JustReference"
 #endif
 #ifndef  JucePlugin_Manufacturer
- #define JucePlugin_Manufacturer           "yourcompany"
+ #define JucePlugin_Manufacturer           "JustMixing"
 #endif
 #ifndef  JucePlugin_ManufacturerWebsite
- #define JucePlugin_ManufacturerWebsite    "www.yourcompany.com"
+ #define JucePlugin_ManufacturerWebsite    "www.JustMixing.com"
 #endif
 #ifndef  JucePlugin_ManufacturerEmail
  #define JucePlugin_ManufacturerEmail      ""
@@ -110,10 +110,10 @@
  #define JucePlugin_AUManufacturerCode     JucePlugin_ManufacturerCode
 #endif
 #ifndef  JucePlugin_CFBundleIdentifier
- #define JucePlugin_CFBundleIdentifier     com.yourcompany.JustReference
+ #define JucePlugin_CFBundleIdentifier     com.JustMixing.JustReference
 #endif
 #ifndef  JucePlugin_AAXIdentifier
- #define JucePlugin_AAXIdentifier          com.yourcompany.JustReference
+ #define JucePlugin_AAXIdentifier          com.JustMixing.JustReference
 #endif
 #ifndef  JucePlugin_AAXManufacturerCode
  #define JucePlugin_AAXManufacturerCode    JucePlugin_ManufacturerCode
@@ -137,7 +137,7 @@
  #define JucePlugin_IAASubType             JucePlugin_PluginCode
 #endif
 #ifndef  JucePlugin_IAAName
- #define JucePlugin_IAAName                "yourcompany: JustReference"
+ #define JucePlugin_IAAName                "JustMixing: JustReference"
 #endif
 #ifndef  JucePlugin_VSTNumMidiInputs
  #define JucePlugin_VSTNumMidiInputs       16
@@ -152,10 +152,10 @@
  #define JucePlugin_ARATransformationFlags  0
 #endif
 #ifndef  JucePlugin_ARAFactoryID
- #define JucePlugin_ARAFactoryID           "com.yourcompany.JustReference.factory"
+ #define JucePlugin_ARAFactoryID           "com.JustMixing.JustReference.factory"
 #endif
 #ifndef  JucePlugin_ARADocumentArchiveID
- #define JucePlugin_ARADocumentArchiveID   "com.yourcompany.JustReference.aradocumentarchive.1.0.0"
+ #define JucePlugin_ARADocumentArchiveID   "com.JustMixing.JustReference.aradocumentarchive.1.0.0"
 #endif
 #ifndef  JucePlugin_ARACompatibleArchiveIDs
  #define JucePlugin_ARACompatibleArchiveIDs  ""
