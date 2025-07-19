deeb3eea4cf3cf302e0bc69c5925103591b1351b
Rasmus Olsen <Rasmus782@gmail.com>
2025-07-19 22:35:16 +0200

    Implementerd Slot Processor class
    I have implemented functions and fields in the Slot Processor class, by taking the things happening in the current audioProcessor and put it inside the slotprocessor.cpp

--- JustReference/Source/SlotsProcessor.cpp
+++ JustReference/Source/SlotsProcessor.cpp
@@ -10,3 +10,61 @@
 
 #include "SlotsProcessor.h"
 
+SlotProcessor::SlotProcessor()
+{
+    formatManager.registerBasicFormats();
+}
+
+SlotProcessor::~SlotProcessor()
+{
+    transportSource.setSource(nullptr);
+    readerSource.reset();
+}
+
+void SlotProcessor::prepareToPlay(double sampleRate, int samplePerBlock)
+{
+    transportSource.prepareToPlay(samplePerBlock, sampleRate);
+}
+
+void SlotProcessor::process(juce::AudioBuffer<float>& buffer, const bool& isReference)
+{
+    transportSource.start();
+    if (isReference)
+    {
+        buffer.clear();
+
+        transportSource.getNextAudioBlock(juce::AudioSourceChannelInfo(buffer));
+    } else
+    {
+        juce::AudioBuffer<float> tempBuffer(buffer.getNumChannels(), buffer.getNumSamples());
+        transportSource.getNextAudioBlock(juce::AudioSourceChannelInfo(tempBuffer));
+    }
+
+    loopingZoneProcessor.process(buffer, transportSource);
+}
+
+void SlotProcessor::loadFile(const juce::File& file)
+{
+    currentFile = file;
+    auto* reader = formatManager.createReaderFor(file);
+    if (reader != nullptr)
+    {
+        transportSource.setSource(nullptr);
+        readerSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+        transportSource.setSource(readerSource.get(), 0, nullptr, reader->sampleRate);
+        thumbnail.setSource(new juce::FileInputSource(file));
+        setFileName(file.getFileNameWithoutExtension());
+    }
+}
+
+std::shared_ptr<const juce::String> SlotProcessor::getFileName() const
+{
+    return std::atomic_load_explicit(&fileName, std::memory_order_acquire);
+}
+
+void SlotProcessor::setFileName(juce::String newFileName)
+{
+    std::shared_ptr<const juce::String> newPtr = std::make_shared<juce::String>(newFileName);
+
+    std::atomic_store_explicit(&fileName, newPtr, std::memory_order_release);
+}
--- JustReference/Source/SlotsProcessor.h
+++ JustReference/Source/SlotsProcessor.h
@@ -10,17 +10,26 @@ public:
 	~SlotProcessor();
 
 	void prepareToPlay(double sampleRate, int samplePerBlock);
-	void process(juce::AudioBuffer<float>& buffer);
+	void process(juce::AudioBuffer<float>& buffer, bool& isReference);
+
+	void loadFile(const juce::File& file);
+	std::shared_ptr<const juce::String> getFileName() const;
+
+	juce::AudioThumbnail& getAudioThumbnail() { return thumbnail; }
+	juce::AudioThumbnailCache& getAudioThumbnailCache() { return thumbnailCache; }
 
 	juce::AudioTransportSource& getTransportSource() { return transportSource; }
 private:
 	juce::AudioTransportSource transportSource;
-
+	juce::File currentFile;
 	juce::AudioFormatManager formatManager;
+	std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+
+	void setFileName(juce::String newFileName);
+	std::shared_ptr<const juce::String> fileName{ std::make_shared<juce::String>("No song loaded...") };
+
 	juce::AudioThumbnailCache thumbnailCache{ 10 };
 	juce::AudioThumbnail thumbnail{ 512, formatManager, thumbnailCache };
 
 	LoopingZoneProcessor loopingZoneProcessor;
-
-
 };
